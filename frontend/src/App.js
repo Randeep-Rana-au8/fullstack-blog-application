04f436b3d8be12403311f9b2c25e24db22931732
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-01-26 22:47:49 +0530

    removed unwanted code, created separate folders for similar files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,31 @@
 import { useEffect } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
-import "./App.css";
 import { add_blog, last_blog } from "./redux/actions/allActions";
 import Navbar from "./components/Navbar/Navbar";
-import LatestBlog from "./components/LatestBlog";
-import AllBlogs from "./components/AllBlogs";
+import LatestBlog from "./components/blog&category/LatestBlog";
+import AllBlogs from "./components/blog&category/AllBlogs";
+import "./App.css";
 
 function App({ state, add_blog, last_blog }) {
-  const allBlogs = state.blogsReducer.blogs;
-  const latestBlog = state.blogsReducer.lastBlog;
+  const { blogs, lastBlog } = state.blogsReducer;
   useEffect(() => {
     async function fetchData() {
       const result = await axios.get("https://myvicharbackend.herokuapp.com/api/posts");
-      console.log(result);
       add_blog(result.data);
       last_blog(result.data);
     }
-
     fetchData();
   }, []);
-
-  console.log(allBlogs);
 
   return (
     <div className="App">
       <Navbar />
       <section>
-        {allBlogs ? (
+        {blogs ? (
           <div className="container">
-            <LatestBlog data={latestBlog} />
-            <AllBlogs data={allBlogs} />
+            <LatestBlog data={lastBlog} />
+            <AllBlogs data={blogs} />
           </div>
         ) : (
           "Loading..."
@@ -48,11 +43,3 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, { add_blog, last_blog })(App);
-// {
-//   state.blogsReducer.blogs.map((blog) => (
-//     <div className="container">
-//       <h1>{blog.title}</h1>
-//       <img height="100px" src={blog.imageUrl} />
-//     </div>
-//   ));
-// }
