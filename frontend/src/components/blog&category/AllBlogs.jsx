cb7d352a60b16ee85e582441753786e1368f93df
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-01-27 20:48:02 +0530

    limits blogs on the homepage

--- frontend/src/components/blog&category/AllBlogs.jsx
+++ frontend/src/components/blog&category/AllBlogs.jsx
@@ -3,12 +3,15 @@ import React from "react";
 const AllBlogs = ({ data }) => {
   return (
     <div className="allBlogs">
-      {data.map((blog) => (
-        <div key={blog.id} className="blog">
-          <h5>{blog.title}</h5>
-          <img height="100px" src={blog.imageUrl} />
-        </div>
-      ))}
+      {data
+        .slice(data.length - 6, data.length - 1)
+        .reverse()
+        .map((blog) => (
+          <div key={blog.id} className="blog">
+            <h5>{blog.title}</h5>
+            <img height="100px" src={blog.imageUrl} />
+          </div>
+        ))}
     </div>
   );
 };
--- frontend/src/components/blog&category/LatestBlog.jsx
+++ frontend/src/components/blog&category/LatestBlog.jsx
@@ -4,7 +4,7 @@ const LatestBlog = ({ data }) => {
   return (
     <div className="latestBlog">
       <img height="120px" src={data.imageUrl} alt="Latest blog image" />
-      <h1>Last blog {data.title}</h1>
+      <h1>{data.title}</h1>
       <p>{data.description}</p>
     </div>
   );
