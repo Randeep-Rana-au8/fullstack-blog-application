4a0aeefb8cefe8d2884ebeeadeaf7a0f7ce177c3
Suryakant-thombre-au8 <suryathombre000@gmail.com>
2021-01-28 11:05:11 +0530

    loing route added with jwt in backend

--- backend/routes/user/user.js
+++ backend/routes/user/user.js
@@ -1,23 +1,52 @@
 const express = require("express");
 const router = express();
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const User = require("../../model/user");
 const Joi = require('joi');
-const { route } = require("../data");
 
 router.post("/RegisterUser", async (req, res) => {
-  console.log(req.body);
-  const { error } = validateUser(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-  const user = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    role: req.body.role ? req.body.role : "user",
-  });
-  await user.save();
-  res.send(user);
+  try {
+    const {email, password} = req.body;
+    const { error } = validateUser(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    let user = await User.findOne({email})
+    if(user) {
+    return res.status(200).json({
+        msg: "User already exists.",
+        code: 400
+      })
+    }
+    user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      role: req.body.role ? req.body.role : "user",
+    });
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt)
+    await user.save();
+    //Payload for the token
+    const payload = {
+        user: {
+            id: user.id
+        }
+    };
+    jwt.sign(payload, "randomString", {expiresIn: 100000}, (err, token) => {
+        res.status(200).json({
+            token,
+            code: 200
+        })
+    }) 
+  } catch (error) {
+    console.error(error);
+        res.status(500).json({
+            message: "Erron in saving",
+            code: 502
+        })
+  }
 });
 
 function validateUser(data) {
@@ -33,7 +62,43 @@ function validateUser(data) {
   return Joi.validate(data, schema);
 }
 
-
+router.post('/login', async (req,res) => {
+  const {email, password} = req.body;
+  try {
+    let user = await User.findOne({email});
+    //checking whether user exists
+    if(!user) {
+        return res.status(200).json({
+            message: "User not exists",
+            code: 404
+        });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) {
+        return res.status(200).json({
+            message: "Incorrect password",
+            code: 402
+        })
+    }
+    const payload = {
+        user: {
+            id: user.id
+        }
+    };
+    jwt.sign(payload, "randomString", {expiresIn: 100000}, (err, token) => {
+        res.status(200).json({
+            token,
+            code: 200
+        })
+    })
+  } catch (error) {
+    console.error(err);
+        res.status(500).json({
+            message: "Erron in Login",
+            code: 502
+        })
+  }
+})
 
 // This is admin only route
 router.get('/allusers', async (req,res) => {
