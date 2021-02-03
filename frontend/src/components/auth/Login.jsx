1eea75f3b8eebdf2226858445eda1dc66e9dc533
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-02-03 18:39:21 +0530

    changed the css properties for login and add post

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,16 +1,13 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { connect } from "react-redux";
 import { setUser } from "../../redux/actions/userActions";
 import { Redirect } from "react-router-dom";
-import './login.css'
-
-
+import "./login.css";
 
 const Login = ({ state, setUser }) => {
-
-  const { user } = state.usersReducer
+  const { user } = state.usersReducer;
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -19,32 +16,36 @@ const Login = ({ state, setUser }) => {
   const onPasswordChange = (event) => setPassword(event.target.value);
 
   const onSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     console.log(email, password);
 
-    axios.post('https://myvicharbackend.herokuapp.com/login', {
-      email: email,
-      password: password
-    })
-    .then(function (response) {
-      console.log(response);
-      setUser(response, ...email, ...password);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    axios
+      .post("https://myvicharbackend.herokuapp.com/login", {
+        email: email,
+        password: password,
+      })
+      .then(function (response) {
+        console.log(response);
+        setUser(response, ...email, ...password);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
-  if (user) return <Redirect to='/homePage' />;
+  if (user) return <Redirect to="/homePage" />;
   return (
-    <div className='mainDiv'>
-      <div className='imgDiv'>Here will be image</div>
-      <form onSubmit={onSubmit} className='loginForm'>
-        <input type="text" required placeholder="email" onChange={onEmailChange} />
-        <input type="password" required placeholder="password" onChange={onPasswordChange}/>
-        <button type="submit">login</button>
-        <div className='singup' >New User?
-        <Link to="/signup">signup</Link>
+    <div className="mainDiv">
+      <div className="imgDiv">Here will be image</div>
+      <form onSubmit={onSubmit} className="loginForm">
+        <input className="loginInput" type="text" required placeholder="email" onChange={onEmailChange} />
+        <input className="loginInput" type="password" required placeholder="password" onChange={onPasswordChange} />
+        <button className="loginButton" type="submit">
+          login
+        </button>
+        <div className="singup">
+          New User?
+          <Link to="/signup">signup</Link>
         </div>
       </form>
     </div>
@@ -55,7 +56,7 @@ const mapStateToProps = (state) => {
   console.log(state);
   return {
     state: state,
-  }
-}
+  };
+};
 
 export default connect(mapStateToProps, { setUser })(Login);
