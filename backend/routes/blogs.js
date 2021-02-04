01179efce7bed5c2cbe67b10a89f2d3c515211bb
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-02-04 13:55:25 +0530

    added new backend file

---
+++ backend/routes/blogs.js
@@ -0,0 +1,51 @@
+const Blog = require("../model/blogPost");
+const express = require("express");
+const app = express();
+const Joi = require("joi");
+
+app.get("/posts", async (req, res) => {
+  const blogs = await Blog.find();
+  res.send(blogs);
+});
+
+app.post("/blog", async (req, res) => {
+  const { error } = validateBlog(req.body);
+  console.log(req.body);
+
+  if (error) return res.status(400).send(error.details[0].message);
+  const blog = new Blog({
+    title: req.body.title,
+    description: req.body.description,
+    likes: req.body.likes ? req.body.likes : 0,
+    comments: req.body.comments ? req.body.comments : 0,
+    author: req.body.author,
+    views: req.body.views ? req.body.views : 0,
+    date: Date.now(),
+    // _id: req.body._id,
+    thumbnail: req.body.thumbnail ? req.body.thumbnail : req.body.imageUrl,
+    category: req.body.category ? req.body.category : "Entertainment",
+  });
+
+  await blog.save();
+  console.log(blog);
+  res.send(blog);
+});
+
+function validateBlog(data) {
+  const schema = {
+    title: Joi.string().min(20).max(100).required(),
+    description: Joi.string().min(50).max(1500).required(),
+    date: Joi.date(),
+    category: Joi.array().required(),
+    views: Joi.number(),
+    likes: Joi.number(),
+    comments: Joi.array(),
+    author: Joi.object().required(),
+    _id: Joi.number(),
+    thumbnail: Joi.string().required(),
+  };
+
+  return Joi.validate(data, schema);
+}
+
+module.exports = app;
