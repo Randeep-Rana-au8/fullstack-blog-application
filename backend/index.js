b179e7436210fe2d4a718f3f8566a7eebae105e3
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-02-04 06:08:42 +0530

    added new data in backend

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Api = require("./routes/data");
+const posts = require("./routes/blogs");
+const post = require("./routes/blog");
+const port = process.env.PORT || 3001;
+const user = require("./routes/user/user");
+const health = require("./routes/health");
+
+const InitiateMongoServer = require("./auth/db");
+
+let app = express();
+app.use(cors({
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+InitiateMongoServer();
+
+app.use("/", user);
+app.use("/", health);
+app.use("/api", Api);
+app.use("/api", posts);
+app.use("/blog", post);
+
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`app is running on port ${port}`);
+});
---
+++ backend/model/blogPost.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    min: 10,
+    max: 50,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    min: 30,
+    max: 1500,
+    trim: true,
+  },
+  imageUrl: {
+    type: String,
+    trim: true,
+  },
+  author: {
+    type: Object,
+    required: true,
+  },
+  category: {
+    type: Array,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  comments: {
+    type: Array,
+    default: [],
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
