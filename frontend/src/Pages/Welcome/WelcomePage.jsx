7516a7bfafd7504837d75446944720fbd0bc9635
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-02-06 13:17:41 +0530

    added 4 sections to welcome page

--- frontend/src/Pages/Welcome/WelcomePage.jsx
+++ frontend/src/Pages/Welcome/WelcomePage.jsx
@@ -4,10 +4,10 @@ import "./WelcomePage.css";
 const WelcomePage = () => {
   return (
     <div>
-      <section className="welcomeSection">
+      <section className="welcomeSection first">
         <div className="info">
           <h1>Explore New Things with Us</h1>
-          <button className="exploreButton">Explore</button>
+          <button className="exploreButton button1">Explore</button>
         </div>
         <div>
           <img
@@ -17,13 +17,44 @@ const WelcomePage = () => {
           />
         </div>
       </section>
-      <section className="welcomeSection">
-        <div className="info">Text</div>
-        <img className="infoImage" src="" alt="blog" />
+      <section className="welcomeSection second">
+        <div>
+          <img
+            className="infoImage"
+            src="https://cdn.pixabay.com/photo/2019/08/06/22/48/artificial-intelligence-4389372__480.jpg"
+            alt="blog"
+          />
+        </div>
+        <div className="info">
+          <h1>Explore Tech Things with Us</h1>
+          <button className="exploreButton button2">Explore</button>
+        </div>
       </section>
-      <section className="welcomeSection">
-        <div className="info">Text</div>
-        <img className="infoImage" src="" alt="blog" />
+      <section className="welcomeSection third">
+        <div className="info">
+          <h1>Explore New Things with Us</h1>
+          <button className="exploreButton button3">Explore</button>
+        </div>
+        <div>
+          <img
+            className="infoImage"
+            src="https://cdn.pixabay.com/photo/2018/02/25/21/34/bada-bagh-3181803__480.jpg"
+            alt="blog"
+          />
+        </div>
+      </section>
+      <section className="welcomeSection forth">
+        <div>
+          <img
+            className="infoImage"
+            src="https://cdn.pixabay.com/photo/2016/11/19/20/16/astronaut-1840936__480.jpg"
+            alt="blog"
+          />
+        </div>
+        <div className="info">
+          <h1>Explore Tech Things with Us</h1>
+          <button className="exploreButton button4">Explore</button>
+        </div>
       </section>
     </div>
   );
