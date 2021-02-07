61a9fec6a9754a972de1fcf3d78ac5f8187d40c0
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-02-07 12:15:38 +0530

    added new styling to homepage

--- frontend/src/components/blog&category/LatestBlog.jsx
+++ frontend/src/components/blog&category/LatestBlog.jsx
@@ -11,7 +11,7 @@ const LatestBlog = ({ data }) => {
             <img style={{ width: "100%" }} src={data.thumbnail} alt="Latest blog" />
           </Link>
           <Link className="Link" to={`/blog/${data._id}`}>
-            <h1>{data.title.length > 55 ? data.title.slice(0, 85) : data.title}...</h1>
+            <h4 style={{ marginTop: "12px" }}>{data.title.length > 55 ? data.title.slice(0, 85) : data.title}...</h4>
           </Link>
           {/* <p>{data.description}</p> */}
           <p>{data.description.length > 400 ? data.description.slice(0, 400) + "..." : data.description}</p>
