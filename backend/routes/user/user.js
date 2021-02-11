7652c871871888b3507e4268f07c300a27f84602
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-02-11 12:31:28 +0530

    created new profile page

--- backend/routes/user/user.js
+++ backend/routes/user/user.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const router = express();
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../../model/user");
-const Joi = require('joi');
-const requireLogin = require('../../middleware/requireLogin')
-const { JWT_SECRET } = require('../../keys/keys')
+const Joi = require("joi");
+const requireLogin = require("../../middleware/requireLogin");
+const { JWT_SECRET } = require("../../keys/keys");
 
 router.post("/RegisterUser", async (req, res) => {
   try {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
     const { error } = validateUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    let user = await User.findOne({email})
-    if(user) {
-    return res.status(200).json({
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(200).json({
         msg: "User already exists.",
-        code: 400
-      })
+        code: 400,
+      });
     }
     user = new User({
       firstName: req.body.firstName,
@@ -26,28 +26,36 @@ router.post("/RegisterUser", async (req, res) => {
       email: req.body.email,
       password: req.body.password,
       role: req.body.role ? req.body.role : "user",
+      gender: {
+        type: String,
+        enum: ["male", "female"],
+        default: "male",
+      },
+      profileImage: req.body.profileImage
+        ? req.body.profileImage
+        : "https://res.cloudinary.com/ranacloud/image/upload/v1613023122/user_ebuz08.png",
     });
     const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(password, salt)
+    user.password = await bcrypt.hash(password, salt);
     await user.save();
     //Payload for the token
     const payload = {
-        user: {
-            id: user.id
-        }
+      user: {
+        id: user.id,
+      },
     };
-    jwt.sign(payload, JWT_SECRET, {expiresIn: 100000}, (err, token) => {
-        res.status(200).json({
-            token,
-            code: 200
-        })
-    }) 
+    jwt.sign(payload, JWT_SECRET, { expiresIn: 100000 }, (err, token) => {
+      res.status(200).json({
+        token,
+        code: 200,
+      });
+    });
   } catch (error) {
     console.error(error);
-        res.status(500).json({
-            message: "Erron in saving",
-            code: 502
-        })
+    res.status(500).json({
+      message: "Erron in saving",
+      code: 502,
+    });
   }
 });
 
@@ -64,54 +72,54 @@ function validateUser(data) {
   return Joi.validate(data, schema);
 }
 
-router.post('/login', async (req,res) => {
-  const {email, password} = req.body;
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
   try {
-    let user = await User.findOne({email});
+    let user = await User.findOne({ email });
     //checking whether user exists
-    if(!user) {
-        return res.status(200).json({
-            message: "User not exists",
-            code: 404
-        });
+    if (!user) {
+      return res.status(200).json({
+        message: "User not exists",
+        code: 404,
+      });
     }
     const isMatch = await bcrypt.compare(password, user.password);
-    if(!isMatch) {
-        return res.status(200).json({
-            message: "Incorrect password",
-            code: 402
-        })
+    if (!isMatch) {
+      return res.status(200).json({
+        message: "Incorrect password",
+        code: 402,
+      });
     }
     const payload = {
-        user: {
-            id: user.id
-        }
+      user: {
+        id: user.id,
+      },
     };
-    jwt.sign(payload, JWT_SECRET, {expiresIn: 100000}, (err, token) => {
-        res.status(200).json({
-            token,
-            code: 200
-        })
-    })
+    jwt.sign(payload, JWT_SECRET, { expiresIn: 100000 }, (err, token) => {
+      res.status(200).json({
+        token,
+        code: 200,
+      });
+    });
   } catch (error) {
     console.error(err);
-        res.status(500).json({
-            message: "Erron in Login",
-            code: 502
-        })
+    res.status(500).json({
+      message: "Erron in Login",
+      code: 502,
+    });
   }
-})
+});
 
-router.get('/myProfile',requireLogin, (req,res) => {
-  console.log(req.user)
-  console.log("working")
-  res.send(req.user)
-})
+router.get("/myProfile", requireLogin, (req, res) => {
+  console.log(req.user);
+  console.log("working");
+  res.send(req.user);
+});
 
 // This is admin only route
-router.get('/allusers', async (req,res) => {
-  const users = await User.find()
-    res.send(users)
-})
+router.get("/allusers", async (req, res) => {
+  const users = await User.find();
+  res.send(users);
+});
 
 module.exports = router;
