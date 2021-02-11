084dfa14ec6f1f2560342521afc6d3477398eb48
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-02-11 10:29:37 +0530

    added new data

--- backend/model/posts.js
+++ backend/model/posts.js
@@ -1,28 +1,43 @@
-const mongoose = require('mongoose')
-const {ObjectId} = mongoose.Schema.Types
-const postSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    body:{
-        type:String,
-        required:true
+    body: {
+      type: String,
+      required: true,
     },
-    photo:{
-        type:String,
-        default:'no photo'
+    imageUrl: {
+      type: String,
+      required: true,
     },
     // likes:[{type:ObjectId,ref:"User"}],
     // comments:[{
     //     text:String,
     //     postedBy:{type:ObjectId,ref:"User"}
     // }],
-    postedBy:{
-       type:ObjectId,
-       ref:"User"
-    }
-},{timestamps:true})
+    postedBy: {
+      type: ObjectId,
+      ref: "User",
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+    category: {
+      type: Array,
+      default: ["Entertainment"],
+    },
+  },
+  { timestamps: true }
+);
 
 // mongoose.model("Post",postSchema)
-module.exports = mongoose.model("Post", postSchema)
+module.exports = mongoose.model("Post", postSchema);
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,50 +2,53 @@ const Post = require("../model/posts.js");
 const express = require("express");
 const app = express();
 const Joi = require("joi");
-const requireLogin = require('../middelware/requireLogin')
+const requireLogin = require("../middelware/requireLogin");
 
-app.post('/createpost',requireLogin,(req,res)=>{
-    const {title,body} = req.body 
-    if(!title || !body){
-      return  res.status(422).json({error:"Plase add all the fields"})
-    }
+app.post("/createpost", requireLogin, (req, res) => {
+  const { title, body, imageUrl } = req.body;
+  if (!title || !body) {
+    return res.status(422).json({ error: "Plase add all the fields" });
+  }
 
-    req.user.password = undefined
-    const post = new Post({
-        title,
-        body,
-        // photo:pic,
-        postedBy:req.user
+  req.user.password = undefined;
+  const post = new Post({
+    title,
+    body,
+    imageUrl,
+    postedBy: req.user,
+  });
+  post
+    .save()
+    .then((result) => {
+      res.json({ post: result });
     })
-    post.save().then(result=>{
-        res.json({post:result})
-    })
-    .catch(err=>{
-        console.log(err)
-    })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.get('/allpost',requireLogin,(req,res)=>{
-    Post.find()
-    .populate("postedBy","_id username")
+app.get("/allpost", requireLogin, (req, res) => {
+  Post.find()
+    .populate("postedBy", "_id username")
     // .populate("comments.postedBy","_id name")
-    .sort('-createdAt')
-    .then((posts)=>{
-        res.json({posts})
-    }).catch(err=>{
-        console.log(err)
+    .sort("-createdAt")
+    .then((posts) => {
+      res.json({ posts });
     })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-app.get('/mypost',requireLogin,(req,res)=>{
-    Post.find({postedBy:req.user._id})
-    .populate("postedBy","_id username")
-    .then(mypost=>{
-        res.json({mypost})
+app.get("/mypost", requireLogin, (req, res) => {
+  Post.find({ postedBy: req.user._id })
+    .populate("postedBy", "_id username")
+    .then((mypost) => {
+      res.json({ mypost });
     })
-    .catch(err=>{
-        console.log(err)
-    })
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = app;
