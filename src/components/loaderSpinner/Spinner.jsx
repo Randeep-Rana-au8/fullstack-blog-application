f711120ee9d81da496320af69518fdc81eeaba2f
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-02-12 12:13:46 +0530

    deleted all files

--- src/components/loaderSpinner/Spinner.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Loader from "react-loader-spinner";
-export default class App extends React.Component {
-  render() {
-    return <Loader type="Puff" color="#00BFFF" height={150} width={150} timeout={300000} />;
-  }
-}
